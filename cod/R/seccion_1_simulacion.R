8fb980cf5fed816983ecec2c5438f02dd539259d
Jose Andrey Prado Rojas <andrey.prado@quantosoluciones.com>
2025-11-07 18:43:01 -0600

    feat: bonos cero cupon

---
+++ cod/R/seccion_1_simulacion.R
@@ -0,0 +1,42 @@
+# Carteras de Inversiones
+
+# Colones
+
+## Bonos Cero Cupón
+b0c_crc_venc <- c(48, 60, 120, 180, 240) # Vencimientos en Años
+b0c_crc_valor <- rep(12000000, length(b0c_crc_venc))
+
+## Bonos Cuponados
+bc_crc_cup_x_ano <- c(6, 3, 1)
+bc_crc_cupones <- c(0.04, 0.06, 0.085)
+bc_crc_venc <- c(144, 180, 216)
+bc_crc_valor <- rep(22000000, length(bc_crc_venc))
+
+## Acciones
+
+acc_crc_n <- 550
+acc_crc_precio_hoy <- 25000
+
+# Dólares
+
+## Bonos Cero Cupón
+
+b0c_dol_venc <- c(48, 144, 204)
+b0c_dol_valor <- rep(16000, length(b0c_dol_venc))
+
+## Bonos Cuponados
+
+bc_dol_cup_x_ano <- 2
+bc_dol_cupones <- 0.04
+bc_dol_venc <- 192
+bc_dol_valor <- 25000
+
+## Acciones
+
+acc_dol_n <- 350
+acc_dol_precio_hoy <- 130
+
+
+
+
+
